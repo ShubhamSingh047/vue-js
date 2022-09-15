a24a6f396548570057f7e6e79e8c37aac55d3752
ShubhamSingh047 <shubham.shubham.singh13@gmail.com>
2022-09-16 01:09:19 +0530

    accessing_Dom

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 new Vue({
     el:'#vue-app',
     data:{
-      name:"shubham",  
+      title:"Hello",
+      link:"https://www.google.com/", 
+      name:"shubham",
+      finishedLink:'<a href="https://www.google.com/">Google</a>'
+    },
+    methods:{
+      chnageTitile:function (event) {
+        this.title=event.target.value;
+      },
+      sayName:function(){
+        this.name = "singh!"
+        return this.name;
+      }
     }
+
 });
